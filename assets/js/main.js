669bd12c1e2a444814e568ba173708f0eb22e4b9
Ricardo Henrique <ricardohenrique1@outlook.com>
2015-05-22 13:25:47 -0300

    Adicionada a função enviar formulario exclusivamente via javascript, adiciona um <textarea> no form de contato para que o cliente insira uma descrição do que quer; foi inserido um seletor no style.css para que o <textarea> tenha a mesma largura do container.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
-	$("#knowledges").accordion({heightStyle:'content'});	 
+	$("#knowledges").accordion({heightStyle:'content'});
 });
+
+function sendMail(){
+	var nome = document.getElementById("nome").value;
+	var email = document.getElementById("email").value;
+	var assunto = document.getElementById("assunto").value;
+	var desc = document.getElementById("desc").value;
+	var body = '<strong>Nome: </strong>'+nome+'<br />'+
+			   '<strong>Email: </strong>'+email+'<br />'+
+			   '<strong>Assunto: </strong>'+assunto+'<br />'+
+			   '<strong>Descição: </strong>'+desc;
+	
+	$.ajax({
+		type:"POST",
+		url:"https://mandrillapp.com/api/1.0/messages/send.json",
+		data:{
+				'key':'-po7dRSIlB4YoFYevstd_w',
+				'message':{
+					'from_email':'ricardohenrique996@gmail.com',
+					'to':[
+						{
+							'email':'ricardohenrique1@outlook.com',
+							'name':'Ricardo Henrique',
+							'type':'to'
+						}
+					],
+					'subject':'[MandrillJS] Portfolio',
+					'html':body
+				}
+			}
+	});
+}
