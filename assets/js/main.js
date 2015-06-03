d0f48cf8c9601df2d873929cf630e6cf68f7ab75
Ricardo Henrique <ricardohenrique1@outlook.com>
2015-06-03 13:18:53 -0300

    Criado os Sprites; Foi salvo as posições de cada sprite; colocado o google analytics assincrono e compactado no arquivos externo de javascript.

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,3 +81,11 @@ $("#video-content-imagens").click(function(){
 		'changeFade':0
 	});
 });
+
+var _gaq = [['_setAccount', 'UA-63633330-1'], ['_trackPageview']];
+(function(d, t){
+	var g = d.createElement(t),
+        s = d.getElementsByTagName(t)[0];
+    g.src = 'http://www.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g, s);
+}(document, 'script'));
